fa9a9b80d051460e16dda1df5002c343efdcbacc
vick <bhudia.vick@gmail.com>
2025-10-18 05:59:17 +0100

    Adding Request Listener, Handling Events, Console errors and network errors

--- Starter/tests/storage/Storage.spec.ts
+++ Starter/tests/storage/Storage.spec.ts
@@ -1,5 +1,6 @@
 import { test, expect } from '@playwright/test';
 
+
 test('Saving storage - correct load', async ({ page }) => {
     const someName = 'Vick';
     const nameField = page.getByRole('textbox', { name: 'Name (Required)' });
@@ -16,6 +17,7 @@ test('Saving storage - correct load', async ({ page }) => {
 
     // use the debugger here to illustrate the structure of storage
     const storage = await page.context().storageState();
+
     const z = 5;
 
 });
