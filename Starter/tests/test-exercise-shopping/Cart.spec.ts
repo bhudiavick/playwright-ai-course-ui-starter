8ec579757fe2fb39e05f42bf1ac0cda5a13a7f5d
vick <bhudia.vick@gmail.com>
2025-10-22 06:55:16 +0100

    Using constructor using class rather then exporting each function

--- Starter/tests/test-exercise-shopping/Cart.spec.ts
+++ Starter/tests/test-exercise-shopping/Cart.spec.ts
@@ -1,21 +1,31 @@
 import { test, expect } from '@playwright/test';
-import * as products from './test-exercise-shopping-pages/ProductsPage'
-import * as cart from './test-exercise-shopping-pages/CartPage'
+import { ProductsPage } from './test-exercise-shopping-pages/ProductsPage'
+import { CartPage } from './test-exercise-shopping-pages/CartPage'
 
 test('Item is added to the shopping cart', async ({ page }) => {
     await page.goto('https://valentinos-magic-beans.click/products');
+    const productsPage = new ProductsPage(page);
+    const cartPage = new CartPage(page);
 
-    const addedProduct = await products.addProductToCart(page, 1);
+
+    const addedProduct = await productsPage.addProductToCart(1);
 
     await page.locator('[data-test-id="header-cart-button"]').getByRole('button').click();
 
-    await cart.assertProduct(page, addedProduct.name!)
+    await cartPage.assertProduct(addedProduct.name!)
+    //if page object method returns an object then would use {subTotal}
+    // export async function getSubTotal(page: Page): Promise<{ subTotal: number }> 
+    // { const subTotalWrapper = page.getByText('Subtotal').locator('..').locator('.font-semibold') 
+    // const subTotalText = await subTotalWrapper.textContent(); 
+    // //return Number(subtotal?.substring(1)) 
+    // //If your site uses commas or spaces in price text, sanitize safely:
+    //  const subTotal = Number(subTotalText?.replace(/[^0-9.]/g, ''));
+    //  return {subTotal}; }
 
-    const subtotal = await cart.getSubTotal(page)
 
-    expect(subtotal).toBe(addedProduct.price)
+    // const {subTotal} = await cart.getSubTotal(page)
+    const subTotal = await cartPage.getSubTotal()
 
-
-
+    expect(subTotal).toBe(addedProduct.price);
 
 })
--- Starter/tests/test-exercise-shopping/test-exercise-shopping-pages/CartPage.ts
+++ Starter/tests/test-exercise-shopping/test-exercise-shopping-pages/CartPage.ts
@@ -1,14 +1,22 @@
 import { Page, expect } from '@playwright/test';
 
-export async function assertProduct(page: Page, heading: string) {
-    const firstProductHeading = page.getByRole('heading', {
+export class CartPage {
+    constructor(private page: Page) {
+
+    }
+
+async  assertProduct(heading: string) {
+    const firstProductHeading = this.page.getByRole('heading', {
         name: heading
     })
     await expect(firstProductHeading).toBeVisible()
 }
 
-export async function getSubTotal(page: Page) {
-    const subTotalWrapper = page.getByText('Subtotal').locator('..').locator('.font-semibold')
-    const subtotal = await subTotalWrapper.textContent();
-    return Number(subtotal?.substring(1))
+async  getSubTotal(): Promise<number> {
+    const subTotalWrapper =this.page.getByText('Subtotal').locator('..').locator('.font-semibold')
+    const subTotalText = await subTotalWrapper.textContent();
+    //return Number(subtotal?.substring(1))
+    //If your site uses commas or spaces in price text, sanitize safely:
+    return Number(subTotalText?.replace(/[^0-9.]/g, ''));
+}
 }
